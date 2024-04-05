f345121dd9ea07a8c5552840a91cccbc6a522812
LucasRenatoMelgaRios <lucasmelgar123@.com>
2024-04-05 15:00:43 -0500

    cositas

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,53 @@ function App() {
         "name": "Chloe Park",
         "img": "https://i.pinimg.com/736x/08/2c/9d/082c9deed067cd9e4799c90e0ed04262.jpg",
         "id": 1,
-        "description": "Vicepresidenta del club de teatro"
+        "description": "Vicepresidenta del club de teatro",
+        "rol" : "estudiante"
       },
 
       {
         "name": "Emma Kim",
         "img": "https://i.pinimg.com/736x/3f/bf/e1/3fbfe1d36c252ffc8b1820d3c4094ff7.jpg",
         "id": 2,
-        "description": "Miembro del club de teatro"
+        "description": "Miembro del club de teatro",
+        "rol" : "estudiante"
       },
       {
         "name": "Yelena",
         "img": "https://i.pinimg.com/736x/2e/3b/cd/2e3bcd58335be6276ea552cc89260d7d.jpg",
         "id": 3,
-        "description": "Prosora del club de teatro"
+        "description": "Prosora del club de teatro",
+        "rol" : "profesora"
       },
       {
         "name": "Hanna",
         "img": "        https://i.pinimg.com/564x/60/11/07/601107f6fba4a06f20c340d5aea0dc1d.jpg",
         "id": 4,
-        "description": "Presidenta del club de teatro"
+        "description": "Presidenta del club de teatro",
+        "rol" : "estudiante"
       },
       {
-        "name": "Desconocida",
+        "name": "Gina",
         "img": "https://i.pinimg.com/736x/cb/74/18/cb74183162f97338ae0c09b7b2f68724.jpg",
         "id": 5,
-        "description": "Nideah"
+        "description": "Una waifu que todos conocen",
+        "rol" : "estudiante"
       },
       {
         "name": "Emily",
-        "img": "https://i.pinimg.com/736x/af/fc/33/affc33bfddf1b94d58f05610b1ce5a3e.jpg",
+        "img": "https://i.pinimg.com/736x/d8/f1/46/d8f1460a2d0c83293f4d9a716e5a5743.jpg",
         "id":6,
-        "description" :"La favorita de mocoloco"
-      }
+        "description" :"La favorita de mocoloco",
+        "rol" : "estudiante"
+      },
+      {
+        "name": "Emily 2",
+        "img": "https://i.pinimg.com/736x/5d/5d/5a/5d5d5aad66abd179dc90c4318d948321.jpg",
+        "id":7,
+        "description" :"La pelirroja de cirlces zzzz",
+        "rol" : "estudiante"
+      },
+      
 
     ]
   }
@@ -50,6 +64,7 @@ function App() {
     <>
       <Cards 
       data={data}
+      
       />
     </>
   )
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,65 +1,65 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
+import { useState } from "react";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 `;
 
-
 const CardsContainer = styled.div`
-    display: flex;
-    max-width: 100%;
-    justify-content: center;
-   gap: 20px;
-    margin: 30px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Define el tamaño mínimo y máximo de las columnas */
+  grid-gap: 10px; /* Espacio entre los elementos */
+  max-width: 100%;
+  margin: 0 auto; /* Margen vertical de 30px y horizontal automático para centrar */
+  align-items: stretch; /* Estirar elementos para que tengan la misma altura */
+  align-content: center; /* Centrar horizontalmente los elementos */
 `;
 
 const Card = styled.div`
-    border-radius: 30px;
-    display: flex;
-    max-width: 220px;
-    flex-direction: column;
-    background-color: #4b277e;
-    border: 2px groove black;
-    overflow: hidden;
-    flex: 1;
+  border-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: #bfbfbf;
+  border: 2px groove black;
+  overflow: hidden;
 `;
 
 const ImageContainer = styled.div`
+  width: 100%;
+  height: 70%;
+  overflow: hidden;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  img {
     width: 100%;
-    height: 300px;
-    overflow: hidden;
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-      img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        transition: transform 0.7s, transform-origin 0.4s; /* Agregar transición para la propiedad transform y transform-origin */
-        cursor: pointer;
-        z-index: 10;
+    height: 100%;
+    object-fit: cover;
+    transition: transform 0.7s, transform-origin 0.4s; /* Agregar transición para la propiedad transform y transform-origin */
+    cursor: pointer;
+    z-index: 10;
 
-
-        &:hover{
-            transform: scale(1.1) translateY(-1%);
-            transform-origin: center center;
-        }
- }
+    &:hover {
+      transform: scale(1.1) translateY(-1%);
+      transform-origin: center center;
+    }
+  }
 `;
 
 const TextContainer = styled.div`
-    width: 100%;
-    text-align: center;
-    font-family: "Comic Neue", cursive;
-    color: #ffffff;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    gap: 14px;
-    p{
-    color: #ffff;
-    }
+  width: 100%;
+  height: 30%;
+  text-align: center;
+  font-family: "Comic Neue", cursive;
+  color: #060404;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  p {
+    color: #000000;
+  }
 `;
 
 const SimpleButton = styled.button`
@@ -71,25 +71,54 @@ const SimpleButton = styled.button`
   border-radius: 20px;
   background-color: #181a7f;
   color: #ffff;
+  border: 2px solid white;
 `;
 
 export const Cards = ({ data }) => {
+    const [isVisible, setIsVisible] = useState(true);
+  
+    function handleClick() {
+        setIsVisible(isVisible => !isVisible);
+    }
+
+    const waifusEstudiantes = data.waifus.filter(waifu =>{  //las llaves permiten escribir más de una linea, pero deben tener el RETURN 
+      return waifu.rol === "estudiante"})
+
+  
     return (
-        <>
-            <CardsContainer>
-                {data.waifus.map((waifu => (
-                    <Card key={waifu.id}>
-                        <ImageContainer>
-                            <img src={waifu.img} alt="" />
-                        </ImageContainer>
-                        <TextContainer>
-                            <h1>{waifu.name}</h1>
-                            <p>{waifu.description}</p>
-                            <SimpleButton>Ver más acerca de {waifu.name} </SimpleButton>
-                        </TextContainer>
-                    </Card>
-                )))}
-            </CardsContainer>
-        </>
-    )
-}
+      <>
+        <SimpleButton onClick={handleClick}>
+          {isVisible ? 'Ocultar cards' : 'Mostrar cards'}
+        </SimpleButton>
+        <CardsContainer>
+          {isVisible && data.waifus.map((waifu) => (
+            <Card key={waifu.id}>
+              <ImageContainer>
+                <img src={waifu.img} alt="" />
+              </ImageContainer>
+              <TextContainer>
+                <h1>{waifu.name}</h1>
+                <p>{waifu.description}</p>
+                <SimpleButton>Ver más acerca de {waifu.name}</SimpleButton>
+              </TextContainer>
+            </Card>
+          ))}
+        </CardsContainer>
+        {/* <h1>Waifus estudiantes:</h1>
+        <CardsContainer>
+            {waifusEstudiantes.map((waifu) =>(
+               <Card key={waifu.id}>
+               <ImageContainer>
+                 <img src={waifu.img} alt="" />
+               </ImageContainer>
+               <TextContainer>
+                 <h1>{waifu.name}</h1>
+                 <p>{waifu.description}</p>
+                 <SimpleButton>Ver más acerca de {waifu.name}</SimpleButton>
+               </TextContainer>
+             </Card>
+            ))}
+        </CardsContainer> */}
+      </>
+    );
+  };
