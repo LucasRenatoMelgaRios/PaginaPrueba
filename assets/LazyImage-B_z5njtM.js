f8335e4bd209c2f9b015c31489145d53b86a2855
LucasRenatoMelgaRios <lucasmelgar123@gmail.com>
2024-08-03 21:58:32 -0500

    Updates

---
+++ assets/LazyImage-B_z5njtM.js
@@ -0,0 +1,6 @@
+import{u as r,j as o}from"./index-De1uokOo.js";const s=r.img`
+  width: 25%;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 5px;
+`,i=({src:t,alt:e})=>o.jsx(s,{src:t,alt:e});export{i as default};
